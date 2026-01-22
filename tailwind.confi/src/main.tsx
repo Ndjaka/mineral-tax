fa243edba7d8dbc5c3eeb936f3923707e2ac304d
eugenendjaka <eugendjaka@gmail.com>
2026-01-22 22:50:35 +0100

    chore: nettoyage complet et organisation des fichiers (docs, backups, scripts)

--- tailwind.confi/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Add class to body to hide static SEO content and show React app
-document.body.classList.add("js-loaded");
-
-createRoot(document.getElementById("root")!).render(<App />);
